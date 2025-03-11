6bb746d99c53bbcd3d2ff86186bb2390f4567cab
konstantin b. <bogucharov.dev@gmail.com>
2025-03-11 21:42:41 +0300

    Add components types

---
+++ src/RadioGroup/types/components.ts
@@ -0,0 +1,33 @@
+/**
+ * Свойства контейнера.
+ */
+interface ContainerProps extends React.ComponentPropsWithoutRef<'span'> {
+  disabled: boolean;
+  block: boolean;
+}
+
+/**
+ * Свойства подписи.
+ */
+interface LabelProps {
+  htmlFor?: string;
+  children?: React.ReactNode;
+  className?: string;
+  required?: boolean;
+  asteriskIcon?: React.ReactNode;
+  asteriskPosition?: AsteriskPosition;
+  asteriskClassName?: string;
+}
+
+type AsteriskPosition = 'left' | 'right';
+
+/**
+ * Свойства индикатора обязательности.
+ */
+interface AsteriskProps {
+  icon?: React.ReactNode;
+  position?: AsteriskPosition;
+  className?: string;
+}
+
+export type { ContainerProps, LabelProps, AsteriskPosition, AsteriskProps };
