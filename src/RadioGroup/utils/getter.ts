a985c162ba074d22c7e808dcc7875a4c00560d23
konstantin b. <bogucharov.dev@gmail.com>
2025-03-11 21:48:25 +0300

    Add methods to utils

---
+++ src/RadioGroup/utils/getter.ts
@@ -0,0 +1,59 @@
+import type { OptionKey, OptionContent, OptionValue, ExtraProps } from '../types';
+
+import { isString, isNumber, isFunction } from './checkType';
+
+/**
+ * Получает ключ опции.
+ */
+export const getOptionKey = <T>(option: T, index: number, getter?: OptionKey<T>) => {
+  if (isString(getter)) return option[getter] as React.Key;
+  if (isFunction(getter)) return getter(option, index);
+
+  if (isString(option) || isNumber(option)) return option;
+
+  return index;
+};
+
+/**
+ * Получает значение опции.
+ */
+export const getOptionValue = <T>(option: T, getter?: OptionValue<T>) => {
+  if (isString(getter)) return option[getter];
+  if (isFunction(getter)) return getter(option);
+
+  return option;
+};
+
+/**
+ * Получает отображаемое содержимое опции.
+ */
+export const getOptionContent = <T>(option: T, index: number, getter?: OptionContent<T>) => {
+  if (isString(getter)) return option[getter] as React.ReactNode;
+  if (isFunction(getter)) return getter(option, index);
+
+  if (isString(option) || isNumber(option)) return option;
+
+  // Избагаем ошибку React "Object are not valid as a React child", если option, например, объект
+  return JSON.stringify(option);
+};
+
+/**
+ * Получает дополнительные свойства опции.
+ */
+export const getExtraProps = (
+  index: number,
+  selectedIndex: number,
+  optionsCount: number
+): ExtraProps => {
+  const lastIndex = optionsCount - 1;
+
+  return {
+    currentIndex: index,
+    lastIndex,
+    selectedIndex,
+    isFirstIndex: index === 0,
+    isLastIndex: index === lastIndex,
+    isSelectedIndex: index === selectedIndex,
+    optionsCount,
+  };
+};
--- src/utils/checkType.ts
+++ src/utils/checkType.ts
@@ -1,2 +1,6 @@
+export const isString = (str: unknown): str is string => typeof str === 'string';
+
+export const isNumber = (num: unknown): num is number => typeof num === 'number';
+
 export const isFunction = (fn: unknown): fn is (...args: never) => unknown =>
   typeof fn === 'function';
