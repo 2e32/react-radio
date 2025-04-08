9a54768c8e6e38025311629f16c5fd084bf80357
konstantin b. <bogucharov.dev@gmail.com>
2025-04-08 22:49:49 +0300

    Update type descriptions

--- src/RadioGroup/types/radioGroup.ts
+++ src/RadioGroup/types/radioGroup.ts
@@ -27,21 +27,52 @@ interface RenderOptionProps<O> {
 }
 
 interface RadioGroupProps<V, O> extends Omit<React.ComponentPropsWithRef<'span'>, 'onChange'> {
+  /**
+   * Значение группы.
+   */
   value?: Nullable<V>;
+  /**
+   * Подпись группы.
+   */
   label?: React.ReactNode;
+  /**
+   * Подсказка.
+   */
   hint?: React.ReactNode;
+  /**
+   * Список опций.
+   */
   options?: O[];
+  /**
+   * Вычисляет ключ опции (для случая, когда значения опций не уникальны).
+   */
   optionKey?: OptionKey<O>;
+  /**
+   * Отображает значение опции.
+   */
   optionContent?: OptionContent<O>;
+  /**
+   * Вычисляет значение опции, возвращаемое при выборе опции (change).
+   */
   optionValue?: OptionValue<O>;
+  /**
+   * Вычисляет должна ли опция быть отключена (в состоянии `disabled`).
+   * @param option Опция из списка.
+   * @returns Возвращает `true`, если опция должна быть отключена.
+   */
   optionDisabled?: (option: O) => boolean;
+  /**
+   * Функция для определения равенства текущего значения `value` и опции (для определения признака
+   * `checked` у опции).
+   *
+   * ⚠️ Используйте `isValueEqualOption` для сравнения `value` и опции, если они в виде объектов или
+   * представлены разными типами. По умолчанию они сравниваются с помощью Object.is.
+   *
+   * @param value Текущее значение.
+   * @param option Опция из списка.
+   * @returns Возвращает `true`, если `value` и опция соответствуют друг другу.
+   */
   isValueEqualOption?: (value: Nullable<V>, option: O) => boolean;
-  disabled?: boolean;
-  readOnly?: boolean;
-  block?: boolean;
-  vertical?: boolean;
-  onChange?: (newValue: V, e: React.ChangeEvent<HTMLInputElement>) => void;
-
   /**
    * CSS-класс `label`.
    */
@@ -50,6 +81,26 @@ interface RadioGroupProps<V, O> extends Omit<React.ComponentPropsWithRef<'span'>
    * CSS-класс `hint`.
    */
   hintClassName?: string;
+  /**
+   * Компонент отключен (опция не может быть выбрана, получение фокуса запрещено).
+   * @default false
+   */
+  disabled?: boolean;
+  /**
+   * Компонент только на чтение (опция не может быть выбрана).
+   * @default false
+   */
+  readOnly?: boolean;
+  /**
+   * Компонент занимает всю ширину контейнера.
+   * @default false
+   */
+  block?: boolean;
+  /**
+   * Опции располагаются по вертикали.
+   * @default false
+   */
+  vertical?: boolean;
   /**
    * Значение должно быть обязательно заполнено (добавляет в `label` знак обязательности).
    * @default false
@@ -69,15 +120,22 @@ interface RadioGroupProps<V, O> extends Omit<React.ComponentPropsWithRef<'span'>
    * CSS-класс контейнера, в котором располагается `asteriskIcon`.
    */
   asteriskClassName?: string;
-
   /**
-   * Отображает иконку переключателя.
+   * Отображает иконку радиокнопки.
    *
-   * ⚠️ Используйте этот метод для собственной стилизации галочки.
+   * ⚠️ Используйте этот метод для собственной стилизации метки.
    */
   renderIcon?: (props: RenderIconProps<O>) => React.ReactNode;
-
+  /**
+   * Отображает опцию.
+   *
+   * ⚠️ Используйте этот метод изменения структуры и стилизации опции (её метки, контента).
+   */
   renderOption?: (props: RenderOptionProps<O>) => React.ReactNode;
+  /**
+   * Вызывается при выборе значения.
+   */
+  onChange?: (newValue: V, e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 type RadioGroupComponent = <V, O>(
