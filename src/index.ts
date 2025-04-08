f8598c214883a08e1490c8ef525409a42ae8733a
konstantin b. <bogucharov.dev@gmail.com>
2025-04-08 22:21:31 +0300

    Fix import types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import Radio from './Radio';
 
-export * from './Radio/types';
+export type { CheckmarkAlign, CheckmarkPosition, RadioProps, RenderIconProps } from './Radio/types';
+export type {
+  AsteriskPosition,
+  OptionContent,
+  OptionKey,
+  OptionValue,
+  RadioGroupProps,
+  OptionProps,
+  ExtraProps,
+  RenderOptionProps,
+} from './RadioGroup/types';
 
 export default Radio;
